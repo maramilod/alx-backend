b94426103d434e7bc2e766f02a090d73f59f33d5
maramilod <marammilod03@gmail.com>
2024-05-22 21:21:58 +0100

    add

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python3
-"""
-h
-e
-y
-"""
+'''Task 3: LRU Caching
+'''
+
+
 from collections import OrderedDict
 from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """Represents an object that allows storing and
-    retrieving items from a dictionary with a LRU
-    removal mechanism when the limit is reached.
-    """
+    '''A class `LRUCache` that inherits from
+       `BaseCaching` and is a caching system
+    '''
+
     def __init__(self):
-        """Initializes the cache.
-        """
+        '''initialize the cache
+        '''
         super().__init__()
         self.cache_data = OrderedDict()
 
